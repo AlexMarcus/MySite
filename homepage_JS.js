44b593405ad0bf61f6905f79b0cbf83c84fe3a3d
almar77 <almar77@aol.com>
2016-07-05 16:17:43 -0400

    external JS

---
+++ homepage_JS.js
@@ -0,0 +1,23 @@
+<!--
+$(document).ready(function() {
+	$("#con-scroll").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#contact").offset().top
+    	}, 800);
+	});
+});
+$(document).ready(function() {
+	$("#edu-scroll").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#education").offset().top
+    	}, 1000);
+	});
+});
+$(document).ready(function() {
+	$("#we-scroll").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#work-experience").offset().top
+    	}, 1000);
+	});
+});
+-->
